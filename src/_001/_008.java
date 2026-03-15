ac0f6972a52ce385050c2af09c3ad6b15e5ec6b0
sgkang234 <sgkang234@gmail.com>
2026-03-16 01:20:28 +0900

    백준 10430

--- src/_001/_008.java
+++ src/_001/_008.java
@@ -12,7 +12,6 @@ public class _008 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
-
         int A = Integer.parseInt(input[0]);
         int B = Integer.parseInt(input[1]);
         int C = Integer.parseInt(input[2]);
