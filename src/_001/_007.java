96cdbde54b93c09a4550214ac1a53179009d1b98
sgkang234 <sgkang234@gmail.com>
2026-03-16 01:16:29 +0900

    백준 18108

---
+++ src/_001/_007.java
@@ -0,0 +1,17 @@
+package _001;
+
+// 26.03.16
+// 백준 18108번
+// 불기 입력 시 서기로 바꾸는 프로그램
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _007 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
+        System.out.println(year - 543);
+    }
+}
