3cc13a8fc47c06fcb6c28a3dc4f72fc6520be82c
sgkang234 <sgkang234@gmail.com>
2026-03-17 12:53:54 +0900

    백준 10171

---
+++ src/_001/_011.java
@@ -0,0 +1,14 @@
+package _001;
+
+// 26.03.17
+// 백준 10171
+// 고양이 출력    \ 문자를 출력하려면 이스케이프 문자 때문에 \\ 로 작성해야 함
+
+public class _011 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
