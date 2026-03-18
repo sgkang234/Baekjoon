b13a3cef0578d12c873789b8e0312415672362bc
sgkang234 <sgkang234@gmail.com>
2026-03-18 12:28:26 +0900

    백준 14681

---
+++ src/_001/_016.java
@@ -0,0 +1,25 @@
+package _001;
+
+// 26.03.18
+// 백준 14681
+
+
+import java.util.Scanner;
+
+public class _016 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        if (x > 0 && y > 0){
+            System.out.println(1);
+        } else if (x < 0 && y > 0) {
+            System.out.println(2);
+        } else if (x < 0 && y < 0) {
+            System.out.println(3);
+        } else{
+            System.out.println(4);
+        }
+    }
+}
