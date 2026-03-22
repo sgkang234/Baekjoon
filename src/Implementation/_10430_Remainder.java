487e6daeb17ffdd319a30d5a4f4f600a834fbeb7
sgkang234 <sgkang234@gmail.com>
2026-03-22 20:12:46 +0900

    10430_Remainder

--- src/Implementation/_10430_Remainder.java
+++ src/Implementation/_10430_Remainder.java
@@ -1,4 +1,4 @@
-package _001;
+package Implementation;
 
 // 26.03.16
 // 백준 10430번
@@ -8,7 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class _008 {
+public class _10430_Remainder {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
