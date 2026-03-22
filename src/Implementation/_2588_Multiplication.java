83b19fdd5f08791ec0b57a862e203539b37e2768
sgkang234 <sgkang234@gmail.com>
2026-03-22 20:16:42 +0900

    _2588_Multiplication

--- src/Implementation/_2588_Multiplication.java
+++ src/Implementation/_2588_Multiplication.java
@@ -1,4 +1,4 @@
-package _001;
+package Implementation;
 
 // 26.03.16
 // 백준 2588
@@ -8,7 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class _009 {
+public class _2588_Multiplication {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(br.readLine());
