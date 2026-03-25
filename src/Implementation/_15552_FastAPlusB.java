a9ddc8262f1f60ba9ba1699e38627c3b6eef521b
sgkang234 <sgkang234@gmail.com>
2026-03-25 21:53:16 +0900

    _15552_FastAPlusB

--- src/Implementation/_15552_FastAPlusB.java
+++ src/Implementation/_15552_FastAPlusB.java
@@ -2,7 +2,7 @@ package Implementation;
 
 // 2026.03.20
 // 백준 15552
-// StringBuilder를 사용해 더 빠른 처리하는 프로그램
+// StringBuilder를 사용해 더 빠르게 처리하는 프로그램
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -21,7 +21,7 @@ public class _15552_FastAPlusB {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            sb.append(a+b).append('\n');
+            sb.append(a+b).append('\n'); // StringBuilder를 통해 계속 입력을 쌓고 이를 한번에 출력해 빠른 처리가 가능하다
         }
         System.out.println(sb);
     }
