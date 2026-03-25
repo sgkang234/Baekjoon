d5bec2ecb8fe0455a3f5d4346bf33dacee0b5442
sgkang234 <sgkang234@gmail.com>
2026-03-25 21:52:02 +0900

    _25304_Receipt

--- src/Implementation/_25304_Receipt.java
+++ src/Implementation/_25304_Receipt.java
@@ -2,7 +2,7 @@ package Implementation;
 
 // 2026.03.20
 // 백준 25304
-// 구매한 총 액의 가격과 종류를 입력한다. 이후 구매한 물건의 값과 개수를 입력한다. 맞으면 YES, 틀리면 NO
+// 구매한 총 액의 가격과 종류를 입력한다. 이후 구매한 물건의 값과 개수를 입력한다. 맞으면 YES, 틀리면 NO를 출력하는 프로그램
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -21,7 +21,7 @@ public class _25304_Receipt {
             int count = Integer.parseInt(input[1]);
             sum += price * count;
         }
-        if (sum == x) {
+        if (sum == x) { // 프로그램으로 계산된 총액과 사용자가 입력한 총액의 값이 같다면
             System.out.println("Yes");
         } else {
             System.out.println("No");
