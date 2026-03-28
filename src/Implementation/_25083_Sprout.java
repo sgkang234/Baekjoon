600dc97d23d92c1fe3ad216ebea69872f9f84acf
sgkang234 <sgkang234@gmail.com>
2026-03-28 23:10:02 +0900

    _25083_Sprout

---
+++ src/Implementation/_25083_Sprout.java
@@ -0,0 +1,16 @@
+package Implementation;
+
+// 26.03.28
+// 백준 5622
+// 새싹 출력하는 프로그램
+
+public class _25083_Sprout {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
