afb210869f11d3cfabb5f7c2eb1830e42101c412
sgkang234 <sgkang234@gmail.com>
2026-03-31 13:44:24 +0900

    _5086_MultiplesAndFactors

---
+++ src/Math/_5086_MultiplesAndFactors.java
@@ -0,0 +1,34 @@
+package Math;
+
+// 26.03.31
+// 백준 2292
+// 입력마다 첫 번째 숫자가 두 번째 숫자의 약수라면 factor를, 배수라면 multiple을, 둘 다 아니라면 neither를 0 0 이면 종료하는 출력하는 프로그램
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _5086_MultiplesAndFactors {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0){
+                System.out.println(sb);
+                break;
+            }
+            if(b%a == 0 ){
+                sb.append("factor\n");
+            } else if(a%b == 0){
+                sb.append("multiple\n");
+            } else {
+                sb.append("neither\n");
+            }
+        }
+    }
+}
