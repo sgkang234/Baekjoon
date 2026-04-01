f82437cc36a034b5c20607612be88eea5966456e
sgkang234 <sgkang234@gmail.com>
2026-04-01 23:55:16 +0900

    _1001_AMinusB

--- src/implementation/_1001_AMinusB.java
+++ src/implementation/_1001_AMinusB.java
@@ -1,20 +1,34 @@
-package Implementation;
+package implementation;
 
 // 26.03.13
 // 백준 1001번
 // 두 정수 A와 B를 입력 받은 다음, A-B를 출력하는 프로그램
 
+/*
+[입력]
+2 1
+
+[출력]
+1
+*/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class _1001_AMinusB {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int A = Integer.parseInt(input[0]);
-        int B = Integer.parseInt(input[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
-        System.out.println(A - B);
+        // 로직
+        int minus = a - b;
+
+        // 출력
+        System.out.println(minus);
     }
 }
