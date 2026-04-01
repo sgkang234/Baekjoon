7f548d321b2adf23cd57eccc751c53b44f93f3fe
sgkang234 <sgkang234@gmail.com>
2026-04-02 01:04:59 +0900

    _2480_ThreeDice

--- src/implementation/_2480_ThreeDice.java
+++ src/implementation/_2480_ThreeDice.java
@@ -1,4 +1,4 @@
-package Implementation;
+package implementation;
 
 // 26.03.19
 // 백준 2480
@@ -9,18 +9,31 @@ package Implementation;
 모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈)×100원의 상금을 받게 된다.
 */
 
+/*
+[입력]
+3 3 6
+
+[출력]
+1300
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class _2480_ThreeDice {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+    public static void main(String[] args) throws IOException {
+        // 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[3];
         for (int i = 0; i < 3; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        // 로직 및 출력
         Arrays.sort(arr); // 정렬
 
         if (arr[0] == arr[2]) { // 같은 눈이 3개
