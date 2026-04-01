e5b20f19884b83e0ff9e14fa5df86ae8733fccf2
sgkang234 <sgkang234@gmail.com>
2026-04-01 13:29:22 +0900

    _27323_Rectangle

---
+++ src/Implementation/_27323_Rectangle.java
@@ -0,0 +1,22 @@
+package Implementation;
+
+// 26.04.01
+// 백준 1978
+// 세로 길이가 A cm, 가로 길이가 B cm 인 아래와 같은 직사각형의 넓이를 구하는 프로그램
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _27323_Rectangle {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 입력
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        // 출력
+        System.out.println(a * b);
+    }
+}
