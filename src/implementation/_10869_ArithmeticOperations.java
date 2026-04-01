2a5e8069898df6b9ef118b14c582e6db07090cd4
sgkang234 <sgkang234@gmail.com>
2026-04-02 00:07:35 +0900

    _10869_ArithmeticOperations

--- src/implementation/_10869_ArithmeticOperations.java
+++ src/implementation/_10869_ArithmeticOperations.java
@@ -1,20 +1,35 @@
-package Implementation;
+package implementation;
 
 // 26.03.15
-// 백준 10869번
+// 백준 10869
 // 두 자연수 A와 B가 주어질 때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램
+
+/*
+[입력]
+7 3
+
+[출력]
+10
+4
+21
+2
+1
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class _10869_ArithmeticOperations {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int a = Integer.parseInt(input[0]);
-        int b = Integer.parseInt(input[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
+        // 출력
         System.out.println(a+b);
         System.out.println(a-b);
         System.out.println(a*b);
