4f684e9a1226a4332cb3b0a3efa950d3119d3635
sgkang234 <sgkang234@gmail.com>
2026-04-02 22:44:49 +0900

    _2292_Beehive

--- src/math/_2292_Beehive.java
+++ src/math/_2292_Beehive.java
@@ -1,4 +1,4 @@
-package Math;
+package math;
 
 // 26.03.31
 // 백준 2292
@@ -7,21 +7,33 @@ package Math;
 1 -> 2 ~ 7 (방 6개 )-> 8 ~19 방 (12개)-> 방이 6*k씩 증가
 */
 
+/*
+[입력]
+13
+
+[출력]
+3
+*/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class _2292_Beehive {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int wantRoom = Integer.parseInt(br.readLine());
+
+        // 로직
         int wall = 1;
         int room = 1;
-
         while (wantRoom > room) {
             room += 6 * wall;
             wall++;
         }
+
+        // 출력
         System.out.println(wall);
     }
 }
