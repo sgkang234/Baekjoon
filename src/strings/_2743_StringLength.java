257f51127338120ed5508787d63dbc2a8b9a16b1
sgkang234 <sgkang234@gmail.com>
2026-04-02 11:10:50 +0900

    _2743_StringLength

--- src/strings/_2743_StringLength.java
+++ src/strings/_2743_StringLength.java
@@ -1,8 +1,16 @@
-package String;
+package strings;
 
 // 26.03.26
 // 백준 2743
 // 알파벳으로만 이루어진 단어를 입력받아, 그 길이를 출력하는 프로그램
+
+/*
+[입력]
+pulljima
+
+[출력]
+8
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,8 +18,11 @@ import java.io.InputStreamReader;
 
 public class _2743_StringLength {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
+
+        // 출력
         System.out.println(s.length()); // 문자열의 길이를 반환하는 메서드
     }
 }
