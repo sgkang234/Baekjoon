b123686a79a60a876eaee4d9096d9ae5c8518926
sgkang234 <sgkang234@gmail.com>
2026-04-02 10:41:57 +0900

    _2562_MaxValue

--- src/implementation/_2562_MaxValue.java
+++ src/implementation/_2562_MaxValue.java
@@ -1,8 +1,25 @@
-package Implementation;
+package implementation;
 
 // 26.03.24
 // 백준 2562
 // 9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램
+
+/*
+[입력]
+3
+29
+38
+12
+57
+74
+40
+85
+61
+
+[출력]
+85
+8
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,10 +27,13 @@ import java.io.InputStreamReader;
 
 public class _2562_MaxValue {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int max = Integer.parseInt(br.readLine());
-        int index = 1;
 
+        // 로직
+        int index = 1;
+        // index가 0부터가 아닌, 1부터 시작하도록 문제를 풀어야 하기에 1 ~ 9 가 아닌 2 ~ 10
         for (int i = 2; i < 10; i++) {
             int num = Integer.parseInt(br.readLine());
             if(max < num){
@@ -21,7 +41,8 @@ public class _2562_MaxValue {
                 index = i;
             }
         }
-        System.out.println(max);
-        System.out.println(index);
+
+        // 출력
+        System.out.println(max + "\n" + index);
     }
 }
