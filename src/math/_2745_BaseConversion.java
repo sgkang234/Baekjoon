f4e43b26821ff977544e1d9b7f7436ed01aad637
sgkang234 <sgkang234@gmail.com>
2026-04-02 16:29:56 +0900

    _2745_BaseConversion

--- src/math/_2745_BaseConversion.java
+++ src/math/_2745_BaseConversion.java
@@ -1,8 +1,16 @@
-package Math;
+package math;
 
 // 26.03.30
 // 백준 2745
 // 진법 전환 프로그램
+
+/*
+[입력]
+ZZZZZ 36
+
+[출력]
+60466175
+*/
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,13 +19,14 @@ import java.util.StringTokenizer;
 
 public class _2745_BaseConversion {
     public static void main(String[] args) throws IOException {
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         String s = st.nextToken();
         int b = Integer.parseInt(st.nextToken());
-        int result = 0;
 
+        // 로직
+        int result = 0;
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             int value;
@@ -29,6 +38,8 @@ public class _2745_BaseConversion {
             }
             result = result * b + value;
         }
+
+        // 출력
         System.out.println(result);
     }
 }
