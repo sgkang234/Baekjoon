7a8744df4a645ebc6f0407ae19fd0d1848a343e9
sgkang234 <sgkang234@gmail.com>
2026-04-02 11:47:36 +0900

    _2941_CroatianAlphabet

--- src/strings/_2941_CroatianAlphabet.java
+++ src/strings/_2941_CroatianAlphabet.java
@@ -1,15 +1,28 @@
-package String;
+package strings;
 
 // 26.03.29
 // 백준 2941
 // 크로아티아 알파벳 해당 문제는 백준에 가서 직접 보는 것을 추천
 
-import java.util.Scanner;
+/*
+[입력]
+ljes=njak
+
+[출력]
+6
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class _2941_CroatianAlphabet {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
+    public static void main(String[] args) throws IOException {
+        // 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        // 로직
         String[] cro = {
                 "c=", "c-", "dz=", "d-",
                 "lj", "nj", "s=", "z="
@@ -19,6 +32,7 @@ public class _2941_CroatianAlphabet {
             s = s.replace(cro[i], "*"); // 하나의 문자로 치환
         }
 
+        // 출력
         System.out.println(s.length());
     }
 }
