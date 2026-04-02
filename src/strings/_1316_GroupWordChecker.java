19c742607e9c36186a60e8a5a7f037698eb83b7d
sgkang234 <sgkang234@gmail.com>
2026-04-02 16:09:14 +0900

    _1316_GroupWordChecker

--- src/strings/_1316_GroupWordChecker.java
+++ src/strings/_1316_GroupWordChecker.java
@@ -1,23 +1,42 @@
-package String;
+package strings;
 
 // 26.03.29
 // 백준 1316
 // 단어 N개를 입력으로 받아 그룹 단어의 개수를 출력하는 프로그램
 
-import java.util.Scanner;
+/*
+[입력]
+5
+ab
+aa
+aca
+ba
+bb
+
+[출력]
+4
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class _1316_GroupWordChecker {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        scanner.nextLine();
+    public static void main(String[] args) throws IOException {
+        // 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int sum = 0;
+
+        // 로직
         for (int i = 0; i < n; i++) {
-            String s = scanner.nextLine();
+            String s = br.readLine();
             sum += isGroup(s);
         }
+        // 출력
         System.out.println(sum);
     }
+
     public static int isGroup(String s) {
         boolean[] alp = new boolean[26];
         char prev = 0;
