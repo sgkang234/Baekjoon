9de808de0776010b37be8dbb6faf20ecfddd5f3a
sgkang234 <sgkang234@gmail.com>
2026-04-03 11:50:26 +0900

    _24264_AlgorithmClass3

--- src/time_complexity/_24264_AlgorithmClass3.java
+++ src/time_complexity/_24264_AlgorithmClass3.java
@@ -4,11 +4,15 @@ package time_complexity;
 // 백준 24264
 // 이중 반복문 구조에서 실행 횟수(n²)와 시간복잡도 (O(n²))를 출력하는 문제
 /*
-for i <- 1 to n
+MenOfPassion(A[], n) {
+    sum <- 0;
+    for i <- 1 to n
         for j <- 1 to n
-            sum <- sum + A[i] × A[j];
-반복문이 n^2번 반복되므로 연산 횟수는 n^2에 따라 비례한다.
-따라서 시간 복잡도는 O(n^2)이다.
+            sum <- sum + A[i] × A[j]; # 코드1
+    return sum;
+}
+반복문이 n²번 반복되므로 연산 횟수는 n^2에 따라 비례한다.
+따라서 시간 복잡도는 O(n²)이다.
 */
 
 /*
